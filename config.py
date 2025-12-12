041975e7efcf4b28ec13702fd97dd941c046b186
Itvc0110 <162940999+Itvc0110@users.noreply.github.com>
2025-12-12 18:11:13 +0700

    Modify batch size and enhance argument parsing
    Updated batch size for inference and ensured proper argument parsing.

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ def get_args():
     parser.add_argument('--checkpoint_path', type=str, default='./checkpoints/LVFace-B_Glint360K.pt', help='Path to the checkpoint file')
     parser.add_argument('--input_dir', type=str, default='./input_images', help='Directory containing input images for inference')
     parser.add_argument('--output_dir', type=str, default='./results', help='Directory to save embeddings')
-    parser.add_argument('--batch_size', default=64, type=int, help='Batch size for inference')
+    parser.add_argument('--batch_size', default=1024, type=int, help='Batch size for inference')
     parser.add_argument('--device', type=str, default='cuda' if torch.cuda.is_available() else 'cpu', help='Device to use (cuda or cpu)')
     parser.add_argument('--swap_color_channel', action='store_true', help='Swap color channels during preprocessing')
     parser.add_argument('--num_workers', default=4, type=int, help='Number of workers for data loading')
@@ -38,4 +38,5 @@ def get_args():
     
     args = parser.parse_args()
     os.makedirs(args.output_dir, exist_ok=True)
+
     return args
